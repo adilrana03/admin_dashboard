b54fe0034ef6ed2b2bc6dfbbedd6b92b729371c1
adilrana03 <adilrana2014@gmail.com>
2023-07-12 19:10:12 +0530

    first commit

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
 function App() {
 	return (
-		<h1 className="text-3xl font-bold underline">
-			App
-		</h1>);
+		<div>
+			<BrowserRouter>
+				<div className='flex relative dark:bg-main-dark-bg'>
+					<div className='fixed right-4 bottom-4' style={{ zIndex: "1000" }}>
+						<TooltipComponent content="Settings" position='top'>
+							<button type="button"
+								className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
+								style={{ background: 'blue', borderRadius: '50%' }}>
+								<FiSettings />
+							</button>
+						</TooltipComponent>
+					</div>
+
+				</div>
+
+			</BrowserRouter>
+
+		</div>
+	)
 }
 
 export default App;

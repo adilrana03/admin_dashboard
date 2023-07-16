a2054130902d884f0e7b47728fe1ae8e235fb7e9
adilrana03 <adilrana2014@gmail.com>
2023-07-16 12:52:11 +0530

    commits

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -4,40 +4,39 @@ import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 function App() {
-	const activeMenu = true;
+    const activeMenu = true;
 
+    return (
+        <div>
+            <BrowserRouter>
+                <div className='flex relative dark:bg-main-dark-bg'>
+                    <div className='fixed right-4 bottom-4' style={{ zIndex: "1000" }}>
+                        <TooltipComponent content="Settings" position='top'>
+                            <button type="button"
+                                className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
+                                style={{ background: 'black', borderRadius: '50%' }}>
+                                <FiSettings />
+                            </button>
+                        </TooltipComponent>
+                    </div>
+                    {
+                        activeMenu ? (
+                            <div>
+                                Sidebar
+                            </div>
+                        ) : (
+                            <div>
+                                Sidebar
+                            </div>
+                        )
+                    }
 
-	return (
-		<div>
-			<BrowserRouter>
-				<div className='flex relative dark:bg-main-dark-bg'>
-					<div className='fixed right-4 bottom-4' style={{ zIndex: "1000" }}>
-						<TooltipComponent content="Settings" position='top'>
-							<button type="button"
-								className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
-								style={{ background: 'black', borderRadius: '50%' }}>
-								<FiSettings />
-							</button>
-						</TooltipComponent>
-					</div>
-					{
-						activeMenu ? (
-							<div>
-								Sidebar
-							</div>
-						) : (
-							<div>
-								Sidebar w-0
-							</div>
-						)
-					}
+                </div>
 
-				</div>
+            </BrowserRouter>
 
-			</BrowserRouter>
-
-		</div>
-	)
+        </div>
+    )
 }
 
 export default App;
